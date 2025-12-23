65f44643a14740255fff937c693626d2e0556bb1
vinayart <vinayart36@gmail.com>
2025-12-23 15:11:01 +0530

    Create Project with svelte and Tailwind css

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to Akshata</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1 class="text-4xl font-bold text-blue-600">
+  Tailwind CSS Working 🎉
+</h1>
